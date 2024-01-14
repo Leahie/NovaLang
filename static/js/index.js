ae2c54486abc45528bc853730c52fcc52c292f6f
Leah Zhang <93287612+Leahie@users.noreply.github.com>
2024-01-14 16:13:38 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,22 @@
 //document.querySelector("h1").innerText="Nova Lang" don't use, just an example of querySelector
+// zoom.js
+// description: zooming in/out functionality
+
+let zoom = true;
+let container = document.getElementById("text");
+let text = document.getElementById("text-corpus");
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case ' ':
+        if (zoom == true){
+            zoom = false;
+            text.style = 'font-size: 1.5vw'
+        }
+
+        else if (zoom == false){
+            zoom = true;
+            text.style = 'font-size: 5vw'
+        }
+    }
+});

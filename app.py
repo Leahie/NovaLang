788e95019d1bac7e816a0f59a7c08cde275efb22
ShabMan01 <manalaishabeer@gmail.com>
2024-02-10 20:13:40 -0500

    fixed "index out of bounds" error
    fixed "index out of bounds" error -- still double check though
    
    problem was line 44: if the sentence length was <6, for example 3, it couldnt go range(6,  3), so error. also, if  length is 6, will go range(6, 6), which leads to a empty inputNumbers, also giving an error.
    
    fix: if <6, make it go range( length-1, length). if =6, make it go range(3, 6)
    
    @Leahie -- line36 -- !CHECK: @LEAH --> not exactly sure if numBlanks is actually the number of blanks; double check porfa

--- app.py
+++ app.py
@@ -21,26 +21,41 @@ def process(txt):
 
 text = 'Pride_and_Prejudice_by_Jane_Austen.txt'
 lines = process(text)
+
 @app.route('/')
 def home():
-  num =  randint(6, 1000)
-  # Word is actually a sentence, because lines is actually a lot of setences  
-  word = lines[num]
-  x = word.split(' ')
 
-  inputNumbers = [i for i in range(6, len(x))]
-  print(inputNumbers)
-  mod = choices(inputNumbers, k=(len(x)//10)+1)
-  print(mod)
-  mod.sort()
-  print(mod)
+  # !NOTE: @LEAH -- In the future all of this will be generated from the LLM 
+  # {{{
 
-  # In the future all of this will be generated from the LLM 
+  num =  randint(6, 1000) #randomly selects an index corresponding to a sentence from the text
+  word = lines[num] # Word is actually a sentence, because lines is actually a lot of setences  
+  x = word.split(' ') #makes the sentence into an array, with each element being split by a space
+  print("x:", x)
+  
+  # !CHECK: @LEAH --> not exactly sure if numBlanks is actually the number of blanks; double check porfa
+  numBlanks = 6 #how many blanks there will be
+  if len(x) < 6: #if the sentence chosen is shorter than 6 words, the number of blanks will be 1 less than word sentence length
+    numBlanks = len(x)-1
+  elif len(x) == 6: #if the sentence chosen is shorter than 6 words, the number of blanks will be 3
+    numBlanks = 3
+  print("numBlanks:", numBlanks)
+
+  inputNumbers = [i for i in range(numBlanks, len(x))] #a list of the indicies from the numBlanks to length of the split sentence
+  print("input:", inputNumbers)
+  
+  mod = choices(inputNumbers, k=(len(x)//10)+1) # !DO: @LEAH --> what does this line do?????? comment porfa
+  mod.sort() #sorts the list of indicies smallest to largest
+  print("mod:", mod)
+
+
   dic = {
     'text': x, 
     'modify-list': mod,
     'modifiers': ['magically', 'organically', 'going']
   }
+  # }}}
+
   return render_template('home.html', data=dic)
 
 app.debug = True

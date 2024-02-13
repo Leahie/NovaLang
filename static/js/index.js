66bed883eddd24319a81f9cf0d8ebba664d7e2c2
Leah Zhang <LeahZhang1595995@gmail.com>
2024-02-12 22:03:54 -0500

    Changed location of code and added color change to nav bar

--- static/js/index.js
+++ static/js/index.js
@@ -151,12 +151,62 @@ window.addEventListener('keydown', e => {
     }
 });
 
-// const express = require("express");
-// const app = express();
 
-// app.use(express.static(__dirname + "/templates"), (_, res, next) => {
-//   res.status(404)
-//   res.sendFile(__dirname + "/404")
-// });
+function darkmode_init()
+      {
+          let darkmodeSwitch = document.querySelector('header .darkmode');
 
-// app.listen(8080);
+          let darkmodeCookie = {
+              set:function(key,value,time,path,secure=false)
+              {
+                  let expires = new Date();
+                  expires.setTime(expires.getTime() + time);
+                  var path   = (typeof path !== 'undefined') ? pathValue = 'path=' + path + ';' : '';
+                  var secure = (secure) ? ';secure' : '';
+
+                  document.cookie = key + '=' + value + ';' + path + 'expires=' + expires.toUTCString() + secure;
+              },
+              get:function()
+              {
+                  let keyValue = document.cookie.match('(^|;) ?darkmode=([^;]*)(;|$)');
+                  return keyValue ? keyValue[2] : null;
+              },
+              remove:function()
+              {
+                  document.cookie = 'darkmode=; Max-Age=0; path=/';
+              }
+          };
+
+
+          if(darkmodeCookie.get() == 'true')
+          {
+              darkmodeSwitch.classList.add('active');
+              document.body.classList.add('darkmode');
+              document.getElementById('text').classList.add('darkmode');
+              document.getElementById('heading').classList.add('darkmode');
+              // div_top_hypers.style = "background-color: #212121;"
+          }
+
+
+          darkmodeSwitch.addEventListener('click', (event) => {
+              event.preventDefault();
+              event.target.classList.toggle('active');
+              document.body.classList.toggle('darkmode');
+              document.getElementById('text').classList.toggle('darkmode');
+              document.getElementById('heading').classList.toggle('darkmode');
+
+              if(document.body.classList.contains('darkmode'))
+              {
+                  darkmodeCookie.set('darkmode','true',2628000000,'/',false);
+              }
+              else
+              {
+                  darkmodeCookie.remove();
+              }
+          });
+      }
+
+      document.addEventListener('DOMContentLoaded',function()
+      {
+          darkmode_init();
+      });

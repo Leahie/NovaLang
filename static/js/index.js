a73425768c472229c0fb2b4716669baaf95972c3
TheoneandonlyAustenJi <156549081+TheoneandonlyAustenJi@users.noreply.github.com>
2024-02-14 15:04:07 -0500

    New 404

--- static/js/index.js
+++ static/js/index.js
@@ -151,12 +151,4 @@ window.addEventListener('keydown', e => {
     }
 });
 
-// const express = require("express");
-// const app = express();
 
-// app.use(express.static(__dirname + "/templates"), (_, res, next) => {
-//   res.status(404)
-//   res.sendFile(__dirname + "/404")
-// });
-
-// app.listen(8080);

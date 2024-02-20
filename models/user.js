17574f996f5340562d1044e1ede2fb3215a60e08
Leah Zhang <LeahZhang1595995@gmail.com>
2024-02-19 19:38:59 -0500

    Packages Working on Log in, lots of packages to be downloaded

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email:{
+        type: String, 
+        required: true, 
+        unique: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose); //adds the username and password fields 
+
+module.exports = mongoose.model('User', UserSchema);
